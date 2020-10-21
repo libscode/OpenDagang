37bb64aef5db9ec61aa0e9cddccc69e2157ecab7
prasetyo yudho <pyudo28@gmail.com>
2020-10-21 13:45:36 +0700

    add notification screen opendagang-hacktoberfest

--- App/Containers/NotifScreen.js
+++ App/Containers/NotifScreen.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
-import { ScrollView, Text } from 'react-native'
-import { connect } from 'react-redux'
+import { TouchableOpacity } from 'react-native'
+import { ScrollView, 
+        Text, 
+        View, 
+        SafeAreaView, 
+        FlatList,
+        Image, }  from 'react-native'
+import { connect }          from 'react-redux'
+import { Images }           from '../Themes'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
@@ -13,12 +20,66 @@ class NotifScreen extends Component {
   //   this.state = {}
   // }
 
+  renderCardNotif() {
+    const data = [
+      {
+      id  :1,
+      name  : 'Yay, kamu dapat diskon 20%!',
+      description : "Segera gunakan diskon-mu untuk belanja barang impian. Belanja sekarang!",
+      image : Images.alertNotif
+      },
+      {
+        id  :2,
+        name  : 'Yay, kamu dapat diskon 50%!',
+        description : "Segera gunakan diskon-mu untuk belanja barang impian. Belanja sekarang!",
+        image : Images.alertNotif
+      },
+      {
+        id  :3,
+        name  : 'Yay, kamu dapat diskon ongkir',
+        description : "Segera gunakan diskon-mu untuk mendapat gratis ongkir pada hari sabtu dan minggu.",
+        image : Images.alertNotif
+      },
+    ]
+    return(
+      <View style={styles.cardContainer}> 
+        <FlatList
+          data={data}
+          showVerticalScrollIndicator={false}
+          renderItem={({item})=>(
+            <TouchableOpacity onPress={()=> this.actionDetail()}>
+            <View style={styles.cardNotif}> 
+                <View style={styles.rowContent}>
+                  <View style={styles.columnImage}>
+                    <Image
+                      source={item.image}
+                      style={styles.imageContent}
+                    />
+                  </View>
+                  <View style={styles.columnContent}>
+                      <Text style={styles.fontTitle}>{item.name}</Text>
+                      <Text style={styles.fontDescription}>{item.description}</Text>
+                    </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item, index)=> index.toString()}
+        />
+    </View>
+    )
+  }
+
   render () {
     return (
-      <ScrollView style={styles.container}>
-        <Text>NotifScreen Container</Text>
-      </ScrollView>
+      <SafeAreaView style={styles.container}>
+        {this.renderCardNotif()}
+      </SafeAreaView>
     )
+  }
+
+  actionDetail() {
+    alert("Hit Detail Promo")
   }
 }
 
--- App/Containers/Styles/NotifScreenStyle.js
+++ App/Containers/Styles/NotifScreenStyle.js
@@ -1,10 +1,53 @@
 import { StyleSheet } from 'react-native'
 import { Colors, Metrics } from '../../Themes/'
+import {
+  responsiveWidth,
+  responsiveHeight,
+} from 'react-native-responsive-dimensions';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Metrics.navBarHeight,
-    backgroundColor: Colors.background
-  }
+    backgroundColor: Colors.grayMedium
+  },
+  cardContainer: {
+    alignItems: 'center'
+  },
+  cardNotif: {
+    height: 90,
+    width: responsiveWidth(95),
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: 'white',
+    marginTop: 20,
+    paddingLeft: 20,
+    backgroundColor: Colors.ricePaper
+  },
+  rowContent: {
+    flexDirection:'row', 
+    flex: 1
+  },
+  imageContent: {
+    width: 40, 
+    height: 40, 
+    marginTop: 15,
+    resizeMode: 'stretch'
+  },
+  columnImage : {
+    flexDirection:'column'
+  },
+  columnContent: {
+    marginLeft: 25, 
+    flexDirection:'column'
+  },
+  fontTitle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginTop: 10
+  }, 
+  fontDescription: {
+    paddingRight: 30
+  },
 })
